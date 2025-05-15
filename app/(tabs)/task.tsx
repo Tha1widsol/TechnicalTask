32c5e45efb166b1570d719de2fb2c86611364f78
Kelvin Lai <laikelvin8@gmail.com>
2025-05-15 22:29:00 +0100

    Add optimisation changes

--- app/(tabs)/task.tsx
+++ app/(tabs)/task.tsx
@@ -1,67 +1,99 @@
-import { StyleSheet, FlatList, Text, TextInput, View} from 'react-native';
+import { StyleSheet, FlatList, Text, TextInput, View } from 'react-native';
 import React, { useEffect, useState } from 'react';
-import ParallaxScrollView from '@/components/ParallaxScrollView';
-import { IconSymbol } from '@/components/ui/IconSymbol';
+
+type User = {
+  id: number;
+  name: string;
+}
 
 export default function TabTwoScreen() {
-  const [users, setUsers] = useState([]);
-  const [filteredUsers, setFilteredUsers] = useState([]);
-  const [query, setQuery] = useState("");
+  const [users, setUsers] = useState<User[]>([]);
+  const [query, setQuery] = useState('')
+
+  const fetchUsers = async () => {
+    try {
+      const response = await fetch('https://jsonplaceholder.typicode.com/users')
+      const data = await response.json()
+      if (response.ok) setUsers(data)
+    } catch (err) {
+      console.log(err)
+    }
+  }
 
   useEffect(() => {
-    fetch("https://jsonplaceholder.typicode.com/users")
-      .then((res) => res.json())
-      .then((data) => setUsers(data));
-  });
+    fetchUsers()
+  }, [])
 
-  useEffect(() => {
-    fetch("https://jsonplaceholder.typicode.com/users")
-      .then((res) => res.json())
-      .then((data) => {
-        const filtered = data.filter((user) =>
-          user.name.toLowerCase().includes(query.toLowerCase())
-        );
-        setFilteredUsers(filtered);
-      });
-  }, [query]); 
+  const filteredUsers = users.filter((user) =>
+    user.name.toLowerCase().includes(query.toLowerCase())
+  )
 
   return (
-    <ParallaxScrollView
-      headerBackgroundColor={{ light: '#D0D0D0', dark: '#353636' }}
-      headerImage={
-        <IconSymbol
-          size={310}
-          color="#808080"
-          name="brain.head.profile.fill"
-          style={styles.headerImage}
-        />
-      }>
-       <View style={styles.container}>
+    <View style={styles.screen}>
+      <View style={styles.searchContainer}>
         <TextInput
           style={styles.input}
           value={query}
           onChangeText={setQuery}
-          placeholder="Search users..."
+          placeholder='Search users...'
+          clearButtonMode='while-editing'
+          autoCorrect={false}
+          keyboardType='default'
         />
-        </View>
-        <FlatList
-          data={filteredUsers}
-          keyExtractor={(item, index) => index.toString()}
-          renderItem={({ item }) => <Text style={styles.user}>{item.name}</Text>}
-        />
-    </ParallaxScrollView>
+      </View>
+
+      <Text>
+        {filteredUsers.length === 0
+          ? 'No results found'
+          : `${filteredUsers.length} result${filteredUsers.length > 1 ? 's' : ''} found`}
+      </Text>
+
+      <FlatList
+        data={filteredUsers}
+        keyExtractor={(item) => item.id.toString()}
+        initialNumToRender={5}
+        renderItem={({ item }) => (
+          <View style={styles.user}>
+            <Text>{item.name}</Text>
+          </View>
+        )}
+        keyboardShouldPersistTaps='handled'
+      />
+    </View>
   );
 }
 
 const styles = StyleSheet.create({
-  headerImage: {
-    color: '#808080',
-    bottom: -90,
-    left: -35,
-    position: 'absolute',
+  screen: {
+    flex: 1,
+    backgroundColor: '#f4f6fa',
+    paddingTop: 50,
+    rowGap: 10,
+    paddingHorizontal: 16,
   },
-  container: {
+
+  searchContainer: {
     flexDirection: 'row',
-    gap: 8,
+    alignItems: 'center',
+    backgroundColor: '#fff',
+    borderRadius: 14,
+    paddingHorizontal: 12,
+    paddingVertical: 8,
+    marginBottom: 12,
   },
+
+  input: {
+    flex: 1,
+    fontSize: 16,
+    color: '#333',
+  },
+
+  user: {
+    backgroundColor: '#fff',
+    padding: 18,
+    borderRadius: 16,
+    marginVertical: 6,
+  },
+
+
 });
